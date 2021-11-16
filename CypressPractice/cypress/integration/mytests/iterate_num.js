6e37d3531b395dda7cb0a2feab2db2f63005638f
Arivarasan01 <93653795+Arivarasan01@users.noreply.github.com>
2021-11-16 16:46:30 +0530

    Add files via upload

---
+++ CypressPractice/cypress/integration/mytests/iterate_num.js
@@ -0,0 +1,19 @@
+var list =  [5];
+
+for (var i = 0; i < list.length;i++) {
+if(list[i] > 55){
+    console.log(list[i] +" is greater than 55");
+
+}
+
+else if(list[i] == 55){
+
+console.log(list[i] +" is equal to 55");
+
+}
+
+else {
+console.log(list[i] +" is lower than 55");
+
+}
+}
