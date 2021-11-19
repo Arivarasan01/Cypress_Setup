3418cf759869a68d3fd8df459a06c6cfd281760d
Arivarasan01 <93653795+Arivarasan01@users.noreply.github.com>
2021-11-19 17:15:26 +0530

    Add files via upload

---
+++ CypressPractice/cypress/integration/mytests/date_modify.js
@@ -0,0 +1,3 @@
+var s=new Date('2008-02-22');
+month = s.getMonth();
+console.log(month);
---
+++ CypressPractice/cypress/integration/mytests/date_new.js
@@ -0,0 +1,7 @@
+let [month, date, year] = new Date().toLocaleDateString("en-US").split("/");
+
+console.log(new Date().toLocaleDateString("en-US"));
+
+console.log(month, date, year);
+
+
